8a7fad33578f0594a278d282773a5b84f801acef
Venkata Pasupuleti <spasupuleti4@bloomberg.net>
2016-09-27 09:57:30 -0400

    removed _params_type method on controller
    removed so that we won't polluting controller with
    helper methods.t

--- lib/strong_resources/controller/mixin.rb
+++ lib/strong_resources/controller/mixin.rb
@@ -1,17 +1,32 @@
 module StrongResources
   module Controller
     module Mixin
-      def self.included(klass)
-        klass.class_eval do
-          extend ClassMethods
-          class << self
-            attr_accessor :_strong_resources
-          end
+      extend ActiveSupport::Concern
+      class JSONParams
+        attr_reader :params
+        def initialize(controller)
+        params_method = if controller.respond_to?(:raw_params) 
+                          :raw_params 
+                        else
+                          :params
+                        end
+
+          @params = controller.send(params_method)
         end
-        klass._strong_resources = {}
+
+        def type
+          params.try(:[], :data).try(:[], :type).try(:to_sym)
+        end
+      end
+
+      included do
+        class_attribute :_strong_resources, instance_writer: false
+        self._strong_resources = {}
       end
 
       def strong_resource
+
+        _params_type = JSONParams.new(self).type
         resource = self.class._strong_resources[_params_type][action_name.to_sym]
         _params = params
         _params = _params.require(resource.require) if resource.require
@@ -20,18 +35,6 @@ module StrongResources
 
       def update_action?
         action_name == 'update'
-      end
-
-      private
-
-      def _params_type
-        params_method = if respond_to?(:raw_params) 
-                          :raw_params 
-                        else
-                          :params
-                        end
-
-        send(params_method).try(:[], :data).try(:[], :type).try(:to_sym)
       end
 
       module ClassMethods
